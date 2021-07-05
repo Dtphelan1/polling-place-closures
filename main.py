a6d48cbe126f9563e004d4dc0ba4c549f5aab75a
Dylan Phelan <dtphelan1@gmail.com>
2021-07-05 18:49:51 -0400

    boilerplate setup of main; requirements now includes linter autopep

---
+++ main.py
@@ -0,0 +1,14 @@
+import argparse
+import pandas as pd
+import numpy as np
+import os
+import sys
+from matplotlib import pyplot as plt
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', default='data_digits_8_vs_9_noisy')
+    parser.add_argument('--n_rows', type=int, default=3)
+    parser.add_argument('--n_cols', type=int, default=3)
+    parser.add_argument('--example_ids_to_show', type=str, default='0,1,2,3,4,5,6,7,8')
+    args = parser.parse_args()
